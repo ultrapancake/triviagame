526f1e33b24105a8db974ad5b5ef2cf80ce5a4fb
Glen <ultrapancake@gmail.com>
2019-09-17 07:29:25 -0600

    Created questions object; added gifs to images folder

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+//Questions object
+
+var questionsObject = {
+    q1: {
+        q: "What weight class is 145 lbs?",
+        posibAns: ["125 lbs", "145 lbs", "160 lbs", "205 lbs"],
+        ans: "145 lbs",
+        imageLoc: "assets/images/welterweight.gif"
+
+    },
+    q2: {
+        q: "Who is the oldest heavyweight champion of all time?",
+        posibAns: ["Lennox Lewis", "Muhammad Ali", "George Foreman", "Joe Frazier"],
+        ans: "George Foreman",
+        imageLoc: "assets/images/oldest.gif"
+    },
+    q3: {
+        q: "Who is the oldest heavyweight champion of all time?",
+        posibAns: ["Lennox Lewis", "Muhammad Ali", "George Foreman", "Joe Frazier"],
+        ans: "George Foreman",
+        imageLoc: "assets/images/oldest.gif"
+    },
+    q4: {
+        q: "Who is the oldest heavyweight champion of all time?",
+        posibAns: ["Lennox Lewis", "Muhammad Ali", "George Foreman", "Joe Frazier"],
+        ans: "George Foreman",
+        imageLoc: "assets/images/oldest.gif"
+    },
+    q5: {
+        q: "Who is the oldest heavyweight champion of all time?",
+        posibAns: ["Lennox Lewis", "Muhammad Ali", "George Foreman", "Joe Frazier"],
+        ans: "George Foreman",
+        imageLoc: "assets/images/oldest.gif"
+    },
+    q6: {
+        q: "Who is the oldest heavyweight champion of all time?",
+        posibAns: ["Lennox Lewis", "Muhammad Ali", "George Foreman", "Joe Frazier"],
+        ans: "George Foreman",
+        imageLoc: "assets/images/oldest.gif"
+    },
+    q7: {
+        q: "Who is the oldest heavyweight champion of all time?",
+        posibAns: ["Lennox Lewis", "Muhammad Ali", "George Foreman", "Joe Frazier"],
+        ans: "George Foreman",
+        imageLoc: "assets/images/oldest.gif"
+    },
+    q8: {
+        q: "Who is the oldest heavyweight champion of all time?",
+        posibAns: ["Lennox Lewis", "Muhammad Ali", "George Foreman", "Joe Frazier"],
+        ans: "George Foreman",
+        imageLoc: "assets/images/oldest.gif"
+    }
+    
+};
